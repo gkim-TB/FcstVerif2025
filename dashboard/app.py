b7cfa64f8035e0128af46aaa8690c46fc045927d
Kim, Gaeun <gaeun.kim0921@gmail.com>
2025-06-17 08:38:24 +0900

    add project_root dir in app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import os, sys
 from datetime import datetime
-from fcstverif.config import year_start, year_end, REGIONS, model
 
 # ✅ project root
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if PROJECT_ROOT not in sys.path:
     sys.path.insert(0, PROJECT_ROOT)
-    
+
+from fcstverif.config import year_start, year_end, REGIONS, model
+
 # ✅ GitHub base raw URL
 GITHUB_RAW_BASE = "https://raw.githubusercontent.com/gkim-TB/FcstVerif2025/main"
 
